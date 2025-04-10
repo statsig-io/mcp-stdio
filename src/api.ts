5901fc5cb023e145744d2e81c77c54c4d5d35447
Oliver Zheng <oliver@statsig.com>
2025-04-10 14:45:32 -0700

    Add initial openapi spec to tool transform

---
+++ src/api.ts
@@ -0,0 +1,102 @@
+import type { OpenAPIObject, ParameterObject, ReferenceObject, SchemaObject } from "openapi3-ts/oas30";
+import { ZodTypeAny, z } from "zod";
+
+export async function fetchOpenApiSpec(specUrl: string): Promise<OpenAPIObject> {
+  const response = await fetch(specUrl);
+  if (!response.ok) {
+    throw new Error(`Error fetching openapi spec. Status: ${response.status}`);
+  }
+  const json = await response.json();
+  return json as OpenAPIObject;
+}
+
+type ApiSchema = Record<string, Record<string, {summary?: string, description?: string, parameters: Record<string, z.ZodType>}>>;
+
+const methods = ["get", "post", "put", "delete", "options", "head", "patch", "trace"] as const;
+
+function ensureParameterObject(parameter: ParameterObject | ReferenceObject): parameter is ParameterObject {
+  return "$ref" in parameter ? false : true;
+}
+
+function convertSchemaObjectToZod(schemaObject: SchemaObject | ReferenceObject): z.ZodType {
+  if ('$ref' in schemaObject) {
+    throw new Error('$ref in param schema not supported');
+  }
+  if (schemaObject.type) {
+    if (Array.isArray(schemaObject.type)) {
+      throw new Error('array type not supported');
+    }
+    let schema: z.ZodType = (() => {
+    switch (schemaObject.type) {
+      case 'integer':
+        return z.number();
+        case 'number':
+          return z.number();
+        case 'string':
+          return z.string();
+        case 'boolean':
+          return z.boolean();
+        case 'object':
+          throw new Error('object param not supported');
+        case 'null':
+          throw new Error('null param not supported');
+        case 'array':
+          if (!schemaObject.items) {
+            throw new Error('array type schema does not have items');
+          }
+          return z.array(convertSchemaObjectToZod(schemaObject.items));
+        default:
+          (schemaObject.type as never);
+          throw new Error(`Unsupported type: ${schemaObject.type}`);
+      }
+    })();
+    if (schemaObject.nullable) {
+      schema = schema.nullable();
+    }
+    return schema;
+  } else if (schemaObject.oneOf) {
+    // zod prefers to know about the array types at compile time. This makes this work, and we can't infer a typescript type which is fine.
+    return z.union(schemaObject.oneOf.map(convertSchemaObjectToZod) as [ZodTypeAny, ZodTypeAny,...ZodTypeAny[]]);
+  } else {
+    throw new Error('unsupported schema object');
+  }
+}
+
+function convertParameterToZod(parameter: ParameterObject): z.ZodType | undefined {
+  if (!parameter.schema) {
+    return undefined;
+  }
+  return convertSchemaObjectToZod(parameter.schema);
+}
+
+function convertParametersToZod(parameters: (ParameterObject | ReferenceObject)[] | undefined): Record<string, z.ZodType> {
+  if (!parameters) {
+    return {};
+  }
+  return Object.fromEntries(
+    parameters
+      .filter(ensureParameterObject)
+      .map(param => [param.name, convertParameterToZod(param)])
+      .filter(([_, zod]) => zod !== undefined)
+  );
+}
+
+export function generateZodSchema(spec: OpenAPIObject): ApiSchema {
+  const schema: ApiSchema = {};
+  for (const [path, pathItem] of Object.entries(spec.paths)) {
+    schema[path] = {};
+
+    for (const method of methods) {
+      const operation = pathItem[method];
+      if (!operation) {
+        continue;
+      }
+      schema[path][method] = {
+        summary: operation.summary,
+        description: operation.description,
+        parameters: convertParametersToZod(operation.parameters),
+      };
+    }
+  }
+  return schema;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { fetchOpenApiSpec } from "./api.js";
+import { generateZodSchema } from "./api.js";
 import { z } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
 
 const STATSIG_API_URL = "https://api.statsig.com/console/v1/gates";
 const STATSIG_EXPERIMENT_API_URL = "https://api.statsig.com/console/v1/experiments";
@@ -152,7 +155,38 @@ server.tool(
   }
 );
 
+async function buildTools(server: McpServer, specUrl: string) {
+  const spec = await fetchOpenApiSpec("https://api.statsig.com/openapi/20240601.json");
+  const schema = generateZodSchema(spec);
+  const toolNames = new Set<string>();
+  for (const path in schema) {
+    for (const method in schema[path]) {
+      // Only do get for now
+      const { summary, parameters } = schema[path][method];
+      const desc = summary ? `${summary} (Call ${method.toUpperCase()} ${path})` : `Get statsig-${method}-${path}`;
+
+      let toolName = desc.replace(/[^a-zA-Z0-9]/g, '-').substring(0, 64);
+      if (toolNames.has(toolName)) {
+        // has to be unique
+        toolName = `${toolName}-${toolNames.size}`;
+      }
+      toolNames.add(toolName);
+
+      server.tool(
+        toolName,
+        desc,
+        parameters,
+        async (params) => {
+          return { content: [{ type: "text", text: "TODO" }] };
+        }
+      );
+    }
+  }
+}
+
+const STATSIG_OPENAPI_URL = "https://api.statsig.com/openapi/20240601.json";
 async function main() {
+  await buildTools(server, STATSIG_OPENAPI_URL);
   const transport = new StdioServerTransport();
   await server.connect(transport);
   console.error("Statsig MCP Server running on stdio");
@@ -162,3 +196,15 @@ main().catch((error) => {
   console.error("Fatal error in main():", error);
   process.exit(1);
 });
+
+async function derp() {
+  const schema = generateZodSchema(await fetchOpenApiSpec(STATSIG_OPENAPI_URL));
+  for (const path in schema) {
+    for (const method in schema[path]) {
+      const { summary, description, parameters } = schema[path][method];
+      console.log(summary, description, JSON.stringify(zodToJsonSchema(z.object(parameters)), null, 2));
+    }
+  }
+}
+
+//derp();
